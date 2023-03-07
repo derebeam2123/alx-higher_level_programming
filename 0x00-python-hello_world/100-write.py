10797332036b133bfe9c620fa38d9b5820c8dee4
nahom <derebeam2123@gmail.com>
2023-03-06 18:58:28 -0800

    task 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of a
-rt is useful - Dora Korpar, 2015-10-1
-9\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1);
