ff38bfde29d67cb5b1a94f57e5447e700bf0ccf4
nahom <derebeam2123@gmail.com>
2023-03-07 05:54:44 -0800

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
 
+        """Check for multiles of 3 and 5 and replace withfrizzbuzz"""
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{}".format(x),end=" ")
