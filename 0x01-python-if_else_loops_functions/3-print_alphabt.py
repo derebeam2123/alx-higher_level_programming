0c08bfa74bd761c106a789047fed722702d9186b
nahom <derebeam2123@gmail.com>
2023-03-07 02:16:07 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for XUP in range(ord("a"), ord("z") + 1):
-    if chr(XUP) != "q" and chr(XUP) != "e":
-        print("{}".format(chr(xup)), end="")
+for xup in range(ord('a'), ord('z') + 1):
+    if chr(xup) != 'q' and chr(xup) != "e":
+        print("{}".format(chr(xup)), end='')
