357cc155469cf53808dce52d79eb8d5d73750f7c
nahom <derebeam2123@gmail.com>
2023-03-07 07:25:43 -0800

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 i = 0
 for x in range(ord('z'), ord('a')-1, -1):
-    print("{}".format(chr(x-i) if i == 32 else i = 32), end="")
+    print("{}".format(chr(x-i) if i = 32 else i = 32), end="")
