302e77b1cb68ab37a2417a4088652e0423ddce13
nahom <derebeam2123@gmail.com>
2023-03-09 00:20:21 -0800

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == " __main__":
+    """print the sum, difference, multiplcation and qoation of 10 and 5."""
+    from calculator_1 import add, sum, mul, div
+    a = 10
+    b = 5
+    print ("{} + {} = {}".format(a, b, add(a, b)))
+    print ("{} - {} = {}".format(a, b, add(a, b)))
+    print ("{} * {} = {}".format(a, b, add(a, b)))
+    print ("{} / {} = {}".format(a, b, add(a, b)))
