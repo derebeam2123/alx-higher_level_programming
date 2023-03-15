f417fa1575f39d544b5d4c03accf636c00c40324
nahom <derebeam2123@gmail.com>
2023-03-15 06:17:31 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list=[]):
     num = 0
     den = 0
     for tup in my_list:
-        num += tup[0]*tup[1]
+        num += tup[0] * tup[1]
         den += tup[1]
-        return (num/den)
+        return (num / den)
