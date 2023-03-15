bbc3eab969c0c8070aace4a717afe6d65c3d8b74
nahom <derebeam2123@gmail.com>
2023-03-15 06:10:44 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
-    for value_dic in list_keys:
-        if value == a_dictionary.get(value_dic):
-            del a_dictionary[value_dic]
-        return (a_dictionary)
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num = 0
+    den = 0
+    for tup in my_list:
+        num += tup[0]*tup[1]
+        den += tup[1]
+        return (num/den)
