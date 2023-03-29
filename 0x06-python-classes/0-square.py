eebdfb7982a96c7551fc5c5aada0a37cb32a91f5
nahom <derebeam2123@gmail.com>
2023-03-28 18:37:25 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Square generation module for Python project 0x06
-"""
+
+"""Define a class Square."""
+
+
+
 class Square:
-    """class defined for square generation
-        """
-        pass
+
+        """Represent a square."""
+
+            pass
