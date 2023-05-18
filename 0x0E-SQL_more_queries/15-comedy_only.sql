8f08a5f4a43455295983d0a943a0e22a4f3be988
nahom <derebeam2123@gmail.com>
2023-05-17 21:31:01 -0700

    task 3

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,12 @@
--- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- displays the number of shows linked to each
+   -- Each record should display:
+      -- tv_shows.title
+   -- Results must be sorted in ascending order by the show title
+   -- The database name will be passed as an argument of the mysql command
 
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE g.`name` = "Comedy"
- ORDER BY t.`title`;
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
