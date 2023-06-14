6efa05ae65ba80d77c59bfe7b6a1df9ddf874308
nahom <derebeam2123@gmail.com>
2023-06-13 17:58:29 -0700

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+	  charPrint (c) {
+		      if (c === undefined) {
+			            this.print();
+			          } else {
+					        for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+					      }
+		    }
+};
+Footer
+© 2023 GitHub, Inc.
+	Footer navigation
+
