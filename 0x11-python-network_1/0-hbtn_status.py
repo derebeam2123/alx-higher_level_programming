6813cc5e40a727711c87e09c8c10f4b02fdfc578
nahom <derebeam2123@gmail.com>
2023-06-30 09:08:29 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status
-"""
+"""  fetches https://intranet.hbtn.io/status  """
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
