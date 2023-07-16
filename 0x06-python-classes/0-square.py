56055cace95987547641b7bc068720c0391ad382
nahom <derebeam2123@gmail.com>
2023-07-16 16:38:55 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
-
+"""empty class Square that defines a square"""
 
 
 class Square:
-
-        """Represent a square."""
-
-            pass
+    """empty class Square that defines a square"""
+    pass
